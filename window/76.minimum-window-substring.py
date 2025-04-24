f19a57c30bcee24fae209936fb2abd476e4f546a
zhangkefan <k3vin.zhang@gmail.com>
2025-04-24 19:39:04 +0800

    feat: 刷题

--- window/76.minimum-window-substring.py
+++ window/76.minimum-window-substring.py
@@ -3,10 +3,26 @@
 #
 # [76] Minimum Window Substring
 #
+from collections import Counter
 
 
 # @lc code=start
 class Solution:
+    def minWindowConsice(self, s: str, t: str) -> str:
+        ans_left, ans_right = -1, len(s)
+        cnt_s = Counter()  # s 子串字母的出现次数
+        cnt_t = Counter(t)  # t 中字母的出现次数
+
+        left = 0
+        for right, c in enumerate(s):  # 移动子串右端点
+            cnt_s[c] += 1  # 右端点字母移入子串
+            while cnt_s >= cnt_t:  # 涵盖
+                if right - left < ans_right - ans_left:  # 找到更短的子串
+                    ans_left, ans_right = left, right  # 记录此时的左右端点
+                cnt_s[s[left]] -= 1  # 左端点字母移出子串
+                left += 1
+        return "" if ans_left < 0 else s[ans_left: ans_right + 1]
+
     def minWindow(self, s: str, t: str) -> str:
         need = {}; window = {}
         for c in t:
