1b8ab29558f54268febc8150368680f78637b637
kevin <k3vin.zhang@gmail.com>
2025-05-06 21:04:46 +0800

    feat: update

---
+++ backtrace/2698.find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=2698 lang=python3
+#
+# [2698] Find the Punishment Number of an Integer
+#
+
+# @lc code=start
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        def dfs(s: str, pos: int, tot: int, target: int) -> bool:
+            if pos == len(s):
+                return tot == target
+            sum = 0
+            for i in range(pos, len(s)):
+                sum = sum * 10 + int(s[i])
+                if sum + tot > target:
+                    break
+                if dfs(s, i + 1, sum + tot, target):
+                    return True
+            return False
+        res = 0
+        for i in range(1, n + 1):
+            if dfs(str(i * i), 0, 0, i):
+                res += i * i
+        return res
+
+        
+# @lc code=end
+
