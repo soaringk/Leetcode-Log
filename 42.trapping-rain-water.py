e85e7f11c45d7f9542139bf6a6f641a8430c1d2e
kevin <k3vin.zhang@gmail.com>
2025-07-14 23:58:08 +0800

    feat: update

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -7,28 +7,21 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height) == 0:
-            return 0
+        n = len(height)
+        pre_max = [0] * n  # pre_max[i] 表示从 height[0] 到 height[i] 的最大值
+        pre_max[0] = height[0]
+        for i in range(1, n):
+            pre_max[i] = max(pre_max[i - 1], height[i])
 
-        left = 0
-        right = len(height) - 1
-        res = 0
+        suf_max = [0] * n  # suf_max[i] 表示从 height[i] 到 height[n-1] 的最大值
+        suf_max[-1] = height[-1]
+        for i in range(n - 2, -1, -1):
+            suf_max[i] = max(suf_max[i + 1], height[i])
 
-        l_max = height[left]
-        r_max = height[right]
-
-        while left <= right:
-            l_max = max(l_max, height[left])
-            r_max = max(r_max, height[right])
-
-            if l_max < r_max:
-                res += l_max - height[left]
-                left += 1
-            else:
-                res += r_max - height[right]
-                right -= 1
-
-        return res
+        ans = 0
+        for h, pre, suf in zip(height, pre_max, suf_max):
+            ans += min(pre, suf) - h  # 累加每个水桶能接多少水
+        return ans
 
 
 # @lc code=end
