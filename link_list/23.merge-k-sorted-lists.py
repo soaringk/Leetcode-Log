ec3b0f4febab8dfe0f47b95443e56e8134385096
kevin <k3vin.zhang@gmail.com>
2025-07-16 18:12:15 +0800

    feat: update

--- link_list/23.merge-k-sorted-lists.py
+++ link_list/23.merge-k-sorted-lists.py
@@ -12,6 +12,10 @@ from itertools import count
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+ListNode.__lt__ = lambda a, b: a.val < b.val  # 让堆可以比较节点大小
+
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         amount = len(lists)
@@ -46,20 +50,17 @@ class Solution:
         return lists[0]
 
     def mergeKListsHeap(self, lists: List[ListNode]) -> ListNode:
-        dummy = cur = ListNode(0)
+        dummy = cur = ListNode(0) # 哨兵节点，作为合并后链表头节点的前一个节点
         q = PriorityQueue()
-        index = count(0)
-        for head in lists:
+        for head in lists: # 把所有非空链表的头节点入堆
             if head:
-                q.put((head.val, next(index), head))
-        while not q.empty():
-            val, _, head = q.get()
-            cur.next = head
-
+                q.put(head)
+        while not q.empty(): # 循环直到堆为空
+            head = q.get() # 剩余节点中的最小节点
+            cur.next = head # 把 node 添加到新链表的末尾
             cur = cur.next
-            head = head.next
-            if head:
-                q.put((head.val, next(index), head))
+            if head.next: # 下一个节点不为空
+                q.put(head.next)
         return dummy.next
 
 # @lc code=end
